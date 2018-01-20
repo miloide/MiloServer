9fb4d5a1a864c5ea9c39a5cfede63077a6b63050
Arjun Rao <mailarjunrao@gmail.com>
2018-01-21 01:37:42 +0530

    Made run_prettify.js local

--- public/scripts/code.js
+++ public/scripts/code.js
@@ -137,7 +137,7 @@ Code.bindClick = function(el, func) {
  */
 Code.importPrettify = function() {
   var script = document.createElement('script');
-  script.setAttribute('src', 'https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js');
+  script.setAttribute('src', 'scripts/run_prettify.js');
   document.head.appendChild(script);
 };
 
