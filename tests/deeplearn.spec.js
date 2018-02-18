da90291a7a782d7b2ee6980cf9ea54b4cb3d8618
Arjun Rao <mailarjunrao@gmail.com>
2018-02-19 03:42:04 +0530

    REFAC: Added test framework

---
+++ tests/deeplearn.spec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var DeepLearn = require('../src/deeplearn')
+
+describe('Testing DeepLearn module', function() {
+  it('Expect DeepLearn.setup to exist', function() {
+    expect(DeepLearn.setup).to.exist;
+  });
+});
+
