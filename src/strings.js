7c28cca876522e24512a92b200992c205f89f026
Arjun Rao <mailarjunrao@gmail.com>
2018-03-08 20:46:27 +0530

    FEA: Milo Toolbox Refresh

--- src/strings.js
+++ src/strings.js
@@ -19,7 +19,7 @@ var MSG = {
   catColour: "Colour",
   catVariables: "Variables",
   catFunctions: "Functions",
-  catML: "Machine Learning",
+  catML: "Advanced",
   listVariable: "list",
   textVariable: "text",
   httpRequestError: "There was a problem with the request.",
