cb77eeefe589e5b1c75ad7cc50b9bdf0c7b9c35d
Arjun Rao <mailarjunrao@gmail.com>
2018-03-20 20:08:45 +0530

    REFAC: Added production settings check

--- config/auth.js
+++ config/auth.js
@@ -2,9 +2,10 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/user');
 var configAuth = require('./credentials');
 var LocalStrategy = require('passport-local').Strategy;
+const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
 
 module.exports = function(passport){
-    passport.use(new LocalStrategy(User.authenticate())); 
+    passport.use(new LocalStrategy(User.authenticate()));
 
     passport.serializeUser(function(user, done) {
         done(null, user.id);
@@ -19,13 +20,13 @@ module.exports = function(passport){
     passport.use(new GoogleStrategy({
         clientID        : configAuth.googleAuth.clientID,
         clientSecret    : configAuth.googleAuth.clientSecret,
-        callbackURL     : configAuth.googleAuth.callbackURL,
+        callbackURL     : NODE_ENV=='development'? configAuth.googleAuth.devCallback:configAuth.googleAuth.prodCallback,
     },
     function(token, refreshToken, profile, done) {
 
         process.nextTick(function() {
 
-            User.findOne({ 'name' : profile.id }, function(err, user) {
+            User.findOne({'name' : profile.id}, function(err, user) {
                 if (err)
                     return done(err);
                 if (user) {
