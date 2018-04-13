5bd44e19a14252b9307cd7b6de09e95a873bab35
arjun-rao <mailarjunrao@gmail.com>
2018-04-13 16:03:57 +0530

    test

--- src/ML/Kmeans.js
+++ src/ML/Kmeans.js
@@ -1,5 +1,5 @@
 
-var kmeans = require('clusters');
+var kmeans = require('./clusters');
 
 var config = {
     k : 3,
@@ -11,28 +11,28 @@ var distances = {
     "euclidean": function(v1, v2) {
         var total = 0;
         for (var i = 0; i < v1.length; i++) {
-           total += Math.pow(v2[i] - v1[i], 2);      
+           total += Math.pow(v2[i] - v1[i], 2);
         }
         return Math.sqrt(total);
      },
      "manhattan": function(v1, v2) {
        var total = 0;
        for (var i = 0; i < v1.length ; i++) {
-          total += Math.abs(v2[i] - v1[i]);      
+          total += Math.abs(v2[i] - v1[i]);
        }
        return total;
      },
      "max": function(v1, v2) {
        var max = 0;
        for (var i = 0; i < v1.length; i++) {
-          max = Math.max(max , Math.abs(v2[i] - v1[i]));      
+          max = Math.max(max , Math.abs(v2[i] - v1[i]));
        }
        return max;
      },
      "min":function(v1, v2){
          var min = 0;
          for (var i = 0; i < v1.length; i++) {
-            min = Math.min(min , Math.abs(v2[i] - v1[i]));      
+            min = Math.min(min , Math.abs(v2[i] - v1[i]));
          }
          return min;
      }
